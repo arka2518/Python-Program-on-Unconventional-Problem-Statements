2356ca05de4c97d6f9028f9899739677220ead61
arka2518 <rehanarka07@gmail.com>
2026-02-01 17:09:18 +0530

    Update Genre Preferrence Program.py

--- Movie Night Chaos Planner/Genre Preferrence Program.py
+++ Movie Night Chaos Planner/Genre Preferrence Program.py
@@ -1,6 +1,6 @@
-Genre = ('Science-friction', 'Horror', 'Superhero', 'Action', 'Adventure', 'Crime-Thriller', 'Comedy', 'Drama', 'Romance', 'Rom-Com', 'Psycological-Thriller')
+Genre = ('Science-fiction', 'Horror', 'Superhero', 'Action', 'Adventure', 'Crime-Thriller', 'Comedy', 'Drama', 'Romance', 'Rom-Com', 'Psycological-Thriller')
 Friends =['Arka', 'Satish', 'Lakshya', 'Avdoot', 'Swaroop', 'Reetul', 'Shaswhat', 'Spondan', 'Sarthak', 'Jay', 'Pratik', 'Anant', 'Harsh', 'Saptadipa', 'Mahi', 'Shantanu', 'Shalabh', 'Shivanee']
-Preferrence = {"Science-friction" : ['Satish', 'Arka', 'Spondan', 'Swaroop'],
+Preference = {"Science-friction" : ['Satish', 'Arka', 'Spondan', 'Swaroop'],
               "Horror" : ['Lakshya', 'Arka'],
               "Rom-Com" : ['Reetul', 'Mahi'],
               "Romance" : ['Shaswhat', 'Reetul', 'Shivanee'],
@@ -17,12 +17,13 @@ print(f"Your friends are: {user_friends}")
 for friend in user_friends:
     if friend not in Friends:
         print(f"No data of genre available for {friend}.")
-    for genre, friends in Preferrence.items():
+    for genre, friends in Preference.items():
         if friend in friends:
             print(f"{friend} likes {genre} genre movies.")
 
 #MAKE SURE YOU TAKE THE INPUT LIKE: Harsh,Jay,Shantanu | THERE SHOULDN'T BE ANY SPACES BETWEEN THE NAMES
 #IF YOU TYPE IT LIKE: Harsh, Jay, Shantanu | IT WOULD ONLY GIVE OUTPUT FOR THE FIRST NAME i.e. 'Harsh'
+
 
 
 
